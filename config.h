cff9c0500d41c8ad4e22843eb58d772f311bfbc6
root <root@ubuntu.(none)>
2015-04-14 15:27:53 +0800

    del some file

--- config.h
+++
@@ -1,6 +0,0 @@
-#ifndef	__CONFIG_H__
-#define	__CONFIG_H__
-
-#define CVS_FILE_PATH	"turn.csv"  // read file
-
-#endif	// __CONFIG_H__
--- dprintf.h
+++
@@ -1,8 +0,0 @@
-#ifndef	__DPRINTF_H__
-#define	__DPRINTF_H__
-
-#include <stdio.h>
-#define DPRINTF(enable, ...) dprintf_impl (__FILE__, __LINE__, __FUNCTION__, enable, ## __VA_ARGS__)
-void dprintf_impl (const char *filename, size_t linenum, const char *funcname, int enable, const char *fmt, ...);
-
-#endif	// __DPRINTF_H__
